13b03af65269d875aa1b24039baaf774c6994056
xvxhordan <xvxhordan@gmail.com>
2021-02-20 20:50:34 -0500

    Se realizo el diseño de la vista de matches en el perfil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,8 +18,25 @@ function Home() {
                             </a>
                         </div>
                     </div>
-                    <div className='fondo-blanco pantalla_match'>
-
+                    <div className='fondo-blanco pantalla_match p-3'>
+                        <div className='div_personas_matches'>
+                            <div className='div_imagen_personas_matches position-relative m-2 d-inline-block'>
+                                <img className='imagen_personas_matches' src={logo} />
+                                <label className='position-absolute label_nombre_matches texto-blanco fw-bold'>Hordan</label>
+                            </div>
+                            <div className='div_imagen_personas_matches position-relative m-2 d-inline-block'>
+                                <img className='imagen_personas_matches' src={logo} />
+                                <label className='position-absolute label_nombre_matches texto-blanco fw-bold'>Hordan</label>
+                            </div>
+                            <div className='div_imagen_personas_matches position-relative m-2 d-inline-block'>
+                                <img className='imagen_personas_matches' src={logo} />
+                                <label className='position-absolute label_nombre_matches texto-blanco fw-bold'>Hordan</label>
+                            </div>
+                            <div className='div_imagen_personas_matches position-relative m-2 d-inline-block'>
+                                <img className='imagen_personas_matches' src={logo} />
+                                <label className='position-absolute label_nombre_matches texto-blanco fw-bold'>Hordan</label>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='col-8 px-0'>
